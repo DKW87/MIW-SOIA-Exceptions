1197acf077c9d2c57a634ff347128015e80fbe11
Danny Kwant <danny.kwant@hva.nl>
2024-06-06 10:07:13 +0200

    Einde opgave 1 - begin opgave 2

--- src/model/Vierkant.java
+++ src/model/Vierkant.java
@@ -8,13 +8,32 @@ public class Vierkant {
 
     // constructors
 
-
+    public Vierkant(double zijde) {
+        this.setZijde(zijde);
+    }
 
     // methoden
 
+    public double getOppervlakte(){
+        return this.zijde * this.zijde;
+    }
 
+    public static double berekenZijde(double oppervlakte){
+        return Math.sqrt(oppervlakte);
+    }
+
+    @Override
+    public String toString() {
+        return "Deze vierkant met een zijde van " + zijde + "heeft een oppervlakte van " + this.getOppervlakte() + "\n";
+    }
 
     // getters en setters
 
+    public double getZijde() {
+        return zijde;
+    }
 
+    public void setZijde(double zijde) {
+        this.zijde = zijde;
+    }
 }
